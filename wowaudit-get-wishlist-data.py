fef9fcf47df71daa8aa9953855e3b6b7b2265b94
Mon3yb <2087287+Mon3yb@users.noreply.github.com>
2026-03-18 23:12:31 +0100

    added teamID retrieval from API

--- wowaudit-get-wishlist-data.py
+++ wowaudit-get-wishlist-data.py
@@ -9,21 +9,17 @@ from pathlib import Path
 wow_path = "" #Path to the _retail_ folder e.g "/home/mon3y/Faugus/battlenet/drive_c/Program Files (x86)/World of Warcraft/_retail_/"
 wow_audit_api_token = "" #Your API token from WoWAudit. Get it from the website (https://wowaudit.com) in the API menu under Credentials
 
-#Your team id. This is a bit hidden. Open the wishlist on the website, then inspect the page, go to the Network tab and search for "teams/"
-#The filepaths include your team id. eg. https://wowaudit.com/api/guilds/11111/teams/<<YOUR-TEAM-ID>>/loot_wishlist.......
-wishlist_team_id = ""
-
 ###########################################################
 ### More variables, normaly you don't need to change these
 
-api_uri = "https://wowaudit.com/v1/rclootcouncil"
+wishlist_api_uri = "https://wowaudit.com/v1/rclootcouncil"
+team_api_uri = "https://wowaudit.com/v1/team"
 addon_path = Path(f"{wow_path}/Interface/AddOns/RCLootCouncil_wowaudit")
 wishlist_path = Path(f"{addon_path}/Data/db.lua")
-
 ###########################################################
 # Functions
 
-def get_api_data(api_uri: str, api_token: str) -> requests.Response:
+def get_wishlist_data(api_uri: str, api_token: str) -> requests.Response:
     headers = {
         "Authorization": f"Bearer {api_token}",
         "Accept": "application/json",
@@ -32,8 +28,7 @@ def get_api_data(api_uri: str, api_token: str) -> requests.Response:
     response = requests.get(api_uri, headers=headers, timeout=30)
 
     if response.status_code == 200:
-        wishlist_data = response.json()
-        return wishlist_data
+        return response.json()
     else:
         subprocess.run([
             "notify-send",
@@ -44,22 +39,46 @@ def get_api_data(api_uri: str, api_token: str) -> requests.Response:
         ], check=False)
         response.raise_for_status()
 
+def get_team_data(api_uri: str, api_token: str) -> requests.Response:
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "Accept": "application/json",
+    }
+
+    response = requests.get(api_uri, headers=headers, timeout=30)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        subprocess.run([
+            "notify-send",
+            "-u","critical",
+            "-a", "WoWAudit wishlist update",
+            f"Error in retrieving team data from WoWAudit",
+            f"{response.status_code} {response.reason} {response.text[:200]}",
+        ], check=False)
+        response.raise_for_status()
+
 def check_updated_timestamp(output_path: str, data:dict) -> bool:
     if Path(output_path).is_file():
         with open(output_path, "r") as f:
             local_db = f.read()
-            local_timestamp = re.search(r'wowauditTimestamp\s\=\s(\d+)', local_db).group(1)
+            local_timestamp = re.search(r'wowauditTimestamp\s\=\s(\d+)', local_db)
             api_timestamp = data['updated_timestamp']
 
-            if int(api_timestamp) > int(local_timestamp):
-                return True
+            if local_timestamp and local_timestamp != None:
+                if int(api_timestamp) > int(local_timestamp.group(1)):
+                    return True
+                else:
+                    return False
             else:
-                return False
+                return True #return true if local_timestamp is not found
 
-def write_to_db_lua(output_path: str, data: dict, team_id: str ) -> None:
-    timestamp = data['updated_timestamp']
-    difficulties = data['difficulties']
-    wishlist_data = data['wishlist_data']
+def write_to_db_lua(output_path: str, wishlist_data: dict, team_data: dict ) -> None:
+    timestamp = wishlist_data['updated_timestamp']
+    difficulties = wishlist_data['difficulties']
+    wishlist_data = wishlist_data['wishlist_data']
+    team_id = team_data['id']
 
     lua_string = (
         f"""wowauditTimestamp = {timestamp}\n"""
@@ -93,28 +112,30 @@ elif not Path(addon_path).is_dir():
     ], check=False)
     raise Exception(f"Addon folder not found\nPlease make sure the Addon RCLootCouncil_wowaudit is installed at {wow_path}")
 else:
-    response = get_api_data(api_uri, wow_audit_api_token)
-    if check_updated_timestamp(wishlist_path, response):
+    wishlist_response = get_wishlist_data(wishlist_api_uri, wow_audit_api_token)
+    team_response = get_team_data(team_api_uri, wow_audit_api_token)
+
+    if check_updated_timestamp(wishlist_path, wishlist_response):
         try:
-            write_to_db_lua(wishlist_path, response, wishlist_team_id)
+            write_to_db_lua(wishlist_path, wishlist_response, team_response)
         except:
             subprocess.run([
                 "notify-send",
                 "-u","critical",
                 "-a", "WoWAudit wishlist update",
                 f"Error in retrieving wishlist data from WoWAudit",
-                f"{response.status_code} {response.reason} {response.text[:200]}",
+                f"{wishlist_response.status_code} {wishlist_response.reason} {wishlist_response.text[:200]}",
             ], check=False)
-            raise Exception(f"Error in retrieving wishlist data from WoWAudit\n{response.status_code} {response.reason} {response.text[:200]}")
+            raise Exception(f"Error in retrieving wishlist data from WoWAudit\n{wishlist_response.status_code} {wishlist_response.reason} {wishlist_response.text[:200]}")
 
         subprocess.run([
             "notify-send",
             "-a", "WoWAudit wishlist update",
             f"Successfully updated wishlist data",
-            f"Total characters: {response['character_amount']}",
-            f"Total wishes: {response['wish_amount']}"
+            f"Total characters: {wishlist_response['character_amount']}",
+            f"Total wishes: {wishlist_response['wish_amount']}"
         ], check=False)
-        print(f"Successfully updated wishlist data\nTotal characters: {response['character_amount']}\nTotal wishes: {response['wish_amount']}")
+        print(f"Successfully updated wishlist data\nTotal characters: {wishlist_response['character_amount']}\nTotal wishes: {wishlist_response['wish_amount']}")
     else:
         subprocess.run([
             "notify-send",
